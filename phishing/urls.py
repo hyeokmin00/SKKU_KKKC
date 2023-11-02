425ef935ca41c06c5c31f4d5e192581cd6b148be
bsnnnnnn <aoohrxx@naver.com>
2023-11-02 20:54:27 +0900

    detail page test

--- phishing/urls.py
+++ phishing/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('')
     path('number_search/', views.number_search, name='nuber_search'),
+    path('mail/', views.SMS_mail, name='SMS_mail'),
+    path('rel_org/', views.rel_org, name='rel_org'),
     path('text/', views.text, name='text')
 ]
--- phishing/views.py
+++ phishing/views.py
@@ -11,5 +11,11 @@ def home(request):
 def number_search(request):
     return render(request, 'number_search.html')
 
+def SMS_mail(request):
+    return render(request, 'SMS_mail.html')
+
 def text(request):
     return render(request, 'text.html')
+
+def rel_org(request):
+    return render(request, 'rel_organization.html')
