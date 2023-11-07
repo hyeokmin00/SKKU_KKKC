8c730998683f09a937d16a689080f58310d92ed5
mindanjichu <a09999@skkukdp.re.kr>
2023-11-07 16:14:03 +0900

    1107 16 commit

--- bopibopi/settings.py
+++ bopibopi/settings.py
@@ -119,13 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-<<<<<<< Updated upstream
-=======
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
->>>>>>> Stashed changes
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
