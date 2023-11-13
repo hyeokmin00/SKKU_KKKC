7d03b9c8430025c568c56b0f0e3508736b3e6312
bsnnnnnn <aoohrxx@naver.com>
2023-11-13 18:43:42 +0900

    model test

--- phishing/views.py
+++ phishing/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
-from .models import Sentence, Text_mail
+from .models import Sentence, Text_mail, Phone_numbers, Account_numbers
 from .utils import load_kcbert_model, calculate_embedding
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
@@ -22,9 +22,17 @@ def home(request):
 def notify(request):
     return render(request, 'notify.html')
 
-# 탐지 결과
-def result(request):
-    return render(request, 'result.html')
+# 번호 조회 결과
+def result_num(request):
+    return render(request, 'result_num.html')
+
+# 모델 결과 : 위험도 낮음
+def result_low(request):
+    return render(request, 'result_low_model.html')
+
+# 모델 결과 : 위험도 높음
+def result_high(request):
+    return render(request, 'result_high_model.html')
 
 # 기관 정보
 def rel_org(request): #기관 전체
@@ -57,8 +65,16 @@ def real_time_detectoin(request):
 
 # 번호 조회 페이지
 def number_search(request):
+    search_number = '' #기본값 설정
     
-    return render(request, 'number_search.html')
+    if request.method == "POST":
+        search_number = request.POST.get('search_number', '')
+
+        # 데이터베이스에서 조회
+        phone_numbers = Phone_numbers.object.filter(phone_number=search_number)
+        account_numbers = Account_numbers.object.filter(account_number=search_number)
+        return render(request, 'result.html', {'phone_numbers':phone_numbers, 'account_numbers':account_numbers, 'search_number':search_number})
+    return render(request, 'number_search.html', {'search_number': search_number})
 
 # 실시간 탐지 시 정보 제공 동의 여부 확인
 def agreement(request):
@@ -99,20 +115,20 @@ class SimilarityView(View):
         kcbert_model, kcbert_tokenizer = load_kcbert_model()
 
         # Calculate embeddings
-        input_embedding = calculate_embedding(input_sentence, kcbert_model, kcbert_tokenizer)
-        input_embedding_normalized = normalize(input_embedding.reshape(1, -1))  # L2 정규화 적용
+        input_embedding = calculate_embedding(input_sentence, kcbert_model, kcbert_tokenizer) 
+        input_embedding_normalized = normalize(input_embedding.reshape(1, -1))  #db 데이터와 비교 위해 정규화 # L2 정규화 적용
 
         # Calculate similarity
         all_text_mails = Text_mail.objects.all()
         similarity_scores = []
 
         # Set a similarity threshold
-        similarity_threshold = 0.5
+        similarity_threshold = 0.5 #유사도 판단 기준값
 
         for idx, text_mail in enumerate(all_text_mails):
             transcript = text_mail.transcript
             if transcript:
-                db_embedding = calculate_embedding(transcript, kcbert_model, kcbert_tokenizer)
+                db_embedding = calculate_embedding(transcript, kcbert_model, kcbert_tokenizer) #input 데이터 임베딩과 같은 방식 사용 필요
                 # db_embedding = np.fromstring(transcript, dtype=float, sep=',')
                 # print(db_embedding)
                 # print(idx)
@@ -120,10 +136,10 @@ class SimilarityView(View):
                     db_embedding_normalized = normalize(db_embedding.reshape(1, -1))  # L2 정규화 적용
                     similarity_score = cosine_similarity(input_embedding_normalized, db_embedding_normalized)[0][0]
                     # print(f"Similarity with '{transcript}': {similarity_score}")
-                    if similarity_score > similarity_threshold:
+                    if similarity_score > similarity_threshold: #유사도 0.5 초과면 유사도 있다고 판단, 리스트에 내용 추가
                         similarity_scores.append({'text': transcript, 'similarity': similarity_score})
 
-        print("Final Similarities:", similarity_scores)
+        print("Final Similarities:", similarity_scores) #html로 결과 반환
         return JsonResponse({'input_sentence': input_sentence, 'similarities': similarity_scores})
 
 
