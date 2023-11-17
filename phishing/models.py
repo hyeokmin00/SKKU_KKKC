ab5304922b7787dd1f08c0cc196f82fe68453552
mindanjichu <a09999@skkukdp.re.kr>
2023-11-17 13:30:04 +0900

    [1116] 오디오 파일 포함, google map 코드 삭제, settings.py 수정

--- phishing/models.py
+++ phishing/models.py
@@ -1,7 +1,24 @@
+# from django.db import models
+
+# # Create your models here.
+# class Organizations(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=20)
+#     image = models.CharField(max_length=255)
+#     call = models.CharField(max_length=50)
+#     work = models.CharField(max_length=100)
+#     url = models.CharField(max_length=255)
+#     category = models.CharField(max_length=20)
+#     number = models.CharField(max_length=20, default="")  # 이후 추가 코드
+
+#     def __str__(self):
+#         return self.name
+
 from django.db import models
 
 
 # Create your models here.
+# 유관기관 정보
 class Organizations(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
@@ -10,3 +27,45 @@ class Organizations(models.Model):
     work = models.CharField(max_length=100)
     url = models.CharField(max_length=255)
     category = models.CharField(max_length=20)
+
+
+# 텍스트 테이블 1. 메일, 문자
+class Text_mail(models.Model):
+    id = models.AutoField(primary_key=True)
+    # filename = models.CharField(max_length=50, null=True)
+    message = models.TextField()
+    label = models.BooleanField(null=True)  # default 설정 가능
+    phone_number = models.CharField(max_length=50, null=True)
+
+
+# 텍스트 테이블 2. KorCCVi
+class Text_KorCCVi(models.Model):
+    id = models.AutoField(primary_key=True)
+    transcript = models.TextField()
+    label = models.BooleanField(null=True)
+
+
+# 음성 파형
+class Wave(models.Model):
+    id = models.AutoField(primary_key=True)
+    spectrogram = models.BigIntegerField()
+
+
+# 번호 1. 전화번호
+class Phone_numbers(models.Model):
+    id = models.AutoField(primary_key=True)
+    phone_number = models.CharField(max_length=50)
+    search_cnt = models.IntegerField()
+
+
+# 번호 2. 계좌번호
+class Account_numbers(models.Model):
+    id = models.AutoField(primary_key=True)
+    account_number = models.CharField(max_length=50)
+    search_cnt = models.IntegerField()
+
+
+################
+class Sentence(models.Model):
+    text = models.CharField(max_length=255)
+    embedding = models.TextField()
